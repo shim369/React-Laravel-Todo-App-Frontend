d15c6d8b07c7f2cfc576838536c96ad1c656e60e
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-14 16:29:57 +0900

    add loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+
+
+function Loading() {
+    return (
+        <div className="conatiner mt-3">
+            <div className="spinner-border spinner-border-sm" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
+}
+
+export default Loading;
