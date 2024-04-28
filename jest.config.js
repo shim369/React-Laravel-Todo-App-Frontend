519771df8d021b1f0a413668778cc09796f44153
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-28 12:15:22 +0900

    update jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.json',
+            isolatedModules: true,
+        },
+    },
+};
