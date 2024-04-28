b7315117788e42025085156accb4a7162e0f9d1e
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-28 11:51:57 +0900

    update test code

--- src/pages/Task.test.tsx
+++ src/pages/Task.test.tsx
@@ -1,10 +1,13 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor, within } from '@testing-library/react';
 import axios from 'axios';
 import { MemoryRouter } from 'react-router-dom';
 import Task from './Task';
 import { Todo } from '../../types/todo';
 
 jest.mock('axios');
+
+const mockAlert = jest.fn();
+window.alert = mockAlert;
 
 const mockTasks: Todo[] = [
   { id: 28, name: 'Python3', url: 'https://github.com/shim369', completed: true },
@@ -14,10 +17,10 @@ const mockTasks: Todo[] = [
 describe('Task Component', () => {
   beforeEach(() => {
     (axios.get as jest.Mock).mockResolvedValue({ data: { tasks: mockTasks } });
-    (axios.delete as jest.Mock).mockResolvedValue({ data: { code: 200, message: 'Task deleted successfully!' } });
+    (axios.delete as jest.Mock).mockResolvedValue({ data: { code: 200, message: 'Task Deleted Successfully!' } });
   });
 
-  test('should render task list', async () => {
+  test('should render task list and delete task', async () => {
     const { getByText } = render(
       <MemoryRouter>
         <Task />
@@ -29,6 +32,18 @@ describe('Task Component', () => {
       expect(getByText('Python3')).toBeInTheDocument();
       expect(getByText('Python')).toBeInTheDocument();
     });
+
+    const row = getByText('Python3').closest('tr');
+    if (row) {
+      const deleteButton = within(row).getByTestId('delete-button-28');
+      fireEvent.click(deleteButton);
+    }
+
+    await waitFor(() => {
+      expect(axios.delete).toHaveBeenCalledWith('http://127.0.0.1:8000/api/delete_task/28');
+      expect(mockAlert).toHaveBeenCalledWith('Task Deleted Successfully!');
+    });
   });
+
 
 });
